fe1dbde430c1666569467ab257324a3682f5d65b
coderdba <gowrishdba@yahoo.com>
2020-11-10 21:15:44 +0530

    more changes

---
+++ sidecar1/telegraf-start-tcp-and-http-listener.sh
@@ -0,0 +1,28 @@
+# Orig from https://thenewstack.io/how-to-setup-influxdb-telegraf-and-grafana-on-docker-part-2/
+#docker run -d --user 998:998 --name=telegraf \
+      #--net=container: \
+      #-e HOST_PROC=/host/proc \
+      #-v /proc:/host/proc:ro \
+      #-v /etc/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
+      #telegraf
+
+chmod 666 /var/run/docker.sock
+
+
+docker run -d --user 998:998 --name=telegraf \
+      --net=monitoring \
+      --memory=1024m \
+      --cpus=2 \
+      --expose=8094/tcp \
+      --expose=8186/tcp \
+      -p 8094:8094/tcp \
+      -p 8186:8186/tcp \
+      -e HOST_PROC=/host/proc \
+      -v /proc:/host/proc:ro \
+      -v /var/run/docker.sock:/var/run/docker.sock:ro \
+      -v /etc/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
+      telegraf
+
+# for exposing udp socket:
+      #--expose=8094/udp \
+      #-p 8094:8094/udp \
